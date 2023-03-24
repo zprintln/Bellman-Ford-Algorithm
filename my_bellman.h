1f9f10cb821eb506541b7a1a6e6ca45c6f7e4593
Patrick Zhang <pzh1pzh@gmail.com>
2023-03-24 02:38:01 -0400

    Implement bellman ford

--- my_bellman.h
+++ my_bellman.h
@@ -1,17 +1,30 @@
 #ifndef BELLMAN_FORD_H
 #define BELLMAN_FORD_H
 
+#include <limits.h>
 
+// Set the max number of vertices
+#define MAX_VERTICES 1000
+// Set the max numebr of edges
+#define MAX_EDGES 10000
 
+// Build a struct to store the edges
+typedef struct {
+    // Start vertex
+    int u;
+    // End vertex
+    int v;
+    // Edge weight
+    int w;
+} Edge;
 
-
-
-
-
-
-
-
-
+// n: the number of vertices
+// m: the number of edges
+// s: source vertex
+// edges[]: an array of Edge structs
+// dist[]: an array of distances
+// dist[i] represnts the distance from source vertex to vertex i
+void bellman_ford(int n, int m, int s, Edge edges[], int dist[]);
 
 
 #endif
