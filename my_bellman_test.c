32e2e970e7da0e2b91591edf90fa2f9eabee70fa
yanginto@gmail.com <yanto1996>
2023-03-30 13:48:01 -0400

    added 2 more unit tests. 1 with negative weight cycle and 1 large graph

--- my_bellman_test.c
+++ my_bellman_test.c
@@ -45,3 +45,56 @@ int unitTest1(){
     freeGraph(graph);
     return 0;
 }
+
+int unitTest2(){
+	int n = 4; // number of vertices
+	int m = 5; // number of edges
+	int s = 0; // source vertex
+	
+	struct Graph* graph = createGraph(n, m);
+	
+	int src[] = {0, 1, 1, 2, 3};
+	int dest[] = {1, 2, 3, 1, 2};
+	int weight[] = {-3, -2, -5, -1, -7};
+	
+	for (int i = 0; i < m; i++){
+		graph->edge[i].src = src[i];
+		graph->edge[i].dest = dest[i];
+		graph->edge[i].weight = weight[i];
+	}
+
+	// Print the graph
+	printf(" 0-----(-3)---->1<----(-2)----3 \n");
+	printf("         |             |        \n");
+	printf("       (-5)           (1)      \n");
+	printf("         |             |        \n");
+	printf("         v             v        \n");
+	printf("         2             4        \n");
+
+	bellmanFord(graph, s);
+
+	freeGraph(graph);
+	return 0;
+}
+
+int unitTest3(){
+	int n = 10;
+	int m = 18;
+	int s = 0;
+
+	struct Graph* graph = createGraph(n,m);
+
+	int src[] = {0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8};
+	int dest[] = {1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 0};
+	int weight[] = {3, 4, -2, 5, -6, 2, 1, 3, -5, -2, 1, 5, 2, 4, 6, 3, 1, 5};
+
+	for (int i = 0; i < m; i++){
+		graph->edge[i].src = src[i];
+		graph->edge[i].dest = dest[i];
+		graph->edge[i].weight = weight[i];
+	}
+
+	bellmanFord(graph, s);
+	freeGraph(graph);
+	return 0;
+}	
